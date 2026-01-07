aeea0e7be860c5e78e78c5c785e2877b124c5698
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-01-08 01:30:46 +0530

    update to kadan's algo prob

--- Arrays-one-dimensional/MaxSubArraySum.java
+++ Arrays-one-dimensional/MaxSubArraySum.java
@@ -1,21 +1,41 @@
+import java.util.*;
+
 public class MaxSubArraySum {
     public static void main (String args[]){
         int[] A = {-2,3,4,-1,5,-10,7};
-        System.out.println(maxSubArrSum(A));
+        ArrayList<Integer> result = maxSubArrSum(A);
+        System.out.println("Max Sum: " + result.get(0));
+        System.out.println("Start Index: " + result.get(1));
+        System.out.println("End Index: " + result.get(2));
     }
 
     //Kadan's Algorithm 
-    public static int maxSubArrSum (int[] A){
+    public static ArrayList<Integer> maxSubArrSum (int[] A){
         int N = A.length;
         int maxSum = Integer.MIN_VALUE;
+        ArrayList<Integer> res = new ArrayList<>();
         int sum  = 0;
+
+        // Initializing start and end index
+        int si = -1, ei = -1; 
+        int tempStart = -1;
         for(int i=0; i<N; i++){
             sum += A[i];
-            maxSum = Math.max(sum, maxSum);
             if(sum < 0){
                 sum = 0;
+                tempStart = i+1;
+            }
+            if(maxSum < sum){
+                maxSum = sum;
+                si = tempStart;
+                ei = i;
             }
         }
-        return maxSum;
+
+        res.add(maxSum);
+        res.add(si );
+        res.add(ei);
+
+        return res;
     }
 }
