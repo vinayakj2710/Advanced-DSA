ac517fe3a825757f2569a5afd49ee772baaa3fa9
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-04-01 04:02:41 +0530

    Max and Min problem - Stacks

---
+++ Stacks/MaxAndMin.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class MaxAndMin{
+    public static void main(String args[]){
+        System.out.println("Calculate and return the sum of (max-min) of all the sub arrays possible");
+    }
+}
