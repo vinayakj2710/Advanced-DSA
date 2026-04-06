be04fff1688c10344ce5b6ea867018ea72c08c39
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-04-07 04:31:46 +0530

    Queue - Implementation using Stack

--- Queues/QueueImplementation.java
+++ Queues/QueueImplementation.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class QueueImplementation {
 
+    //Implementation using Array
+
     static class Queue{
         int[] arr = new  int[5];
         int front = -1, rear = -1;
@@ -43,6 +45,58 @@ class QueueImplementation {
             return size == 0;
         }
     }
+
+    // Implementation using stack
+
+    static class Queue2{
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+
+        void enqueue (int x){
+            st1.push(x);
+        }
+
+        int dequeue(){
+            if (st1.isEmpty() && st2.isEmpty()) {
+                System.out.println("Queue is empty!!");
+                return -1;
+            }
+
+            if(!st2.isEmpty()){
+                return st2.pop();
+            }else{
+                while(!st1.isEmpty()){
+                    st2.push(st1.pop());
+                }
+                return st2.pop();
+            }
+        }
+
+        int peek(){
+            if (st1.isEmpty() && st2.isEmpty()) {
+                System.out.println("Queue is empty!!");
+                return -1;
+            }
+
+            if(!st2.isEmpty()){
+                return st2.peek();
+            }else{
+                while(!st1.isEmpty()){
+                    st2.push(st1.pop());
+                }
+                return st2.peek();
+            }
+        }
+
+        int size(){
+            return st1.size() + st2.size();
+        }
+
+        boolean isEmpty(){
+            return st1.size()+st2.size() == 0;
+        }
+    }
+
     public static void main(String args[]){
         System.out.println("Implementation of Queue using Array");
 
@@ -56,5 +110,38 @@ class QueueImplementation {
         System.out.println("Dequeued: " + q.dequeue());
         System.out.println("Front after dequeue: " + q.front());
         System.out.println("Size: " + q.size());
+
+        // ===============================
+        // Testing Queue using Stacks
+        // ===============================
+
+        System.out.println("\nImplementation of Queue using Stacks");
+
+        Queue2 qs = new Queue2();
+
+        // Enqueue elements
+        qs.enqueue(100);
+        qs.enqueue(200);
+        qs.enqueue(300);
+
+        System.out.println("Front (peek): " + qs.peek());
+
+        System.out.println("Dequeued: " + qs.dequeue());
+
+        System.out.println("Front after dequeue: " + qs.peek());
+
+        System.out.println("Size: " + qs.size());
+
+        // Dequeue all
+        while (!qs.isEmpty()) {
+            System.out.println("Removing: " + qs.dequeue());
+        }
+
+        // Edge case test
+        System.out.println("Trying to dequeue from empty Queue2:");
+        qs.dequeue();
+
+        System.out.println("Trying to peek from empty Queue2:");
+        qs.peek();
     }
 }
