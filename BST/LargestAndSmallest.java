9949aa92323e8768592b93da9d18b2fb73a83f60
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-04-10 03:06:11 +0530

    Find the smallest element in the BST

---
+++ BST/LargestAndSmallest.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class LargestAndSmallest{
+    static class Node{
+        int val;
+        Node right, left;
+
+        Node(int x){
+            val = x;
+            right = left = null;
+        }
+    }
+
+    Node root;
+
+    /* Find smallest value in BST */
+    public static int findSmallest(Node A){
+        if(A == null) return -1;
+        Node temp = A;
+        while(temp.left != null){
+            temp = temp.left;
+        }
+        return temp.val;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Finding the Largest and Smallest in BST");
+
+        LargestAndSmallest tree = new LargestAndSmallest();
+
+        Node root = new Node(10);
+        root.left = new Node(5);
+        root.right = new Node(15);
+        root.left.left = new Node(2);
+        root.left.right = new Node(7);
+
+        System.out.println("Smallest in BST: "+ findSmallest(root));
+    }
+}
