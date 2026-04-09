11252c5ebc035131051a0ad6224e7893437b3476
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-04-10 03:07:57 +0530

    Find the largest in BST

--- BST/LargestAndSmallest.java
+++ BST/LargestAndSmallest.java
@@ -23,6 +23,16 @@ class LargestAndSmallest{
         return temp.val;
     }
 
+    /* Find Largest value in BST */
+    public static int findLargest(Node A){
+        if(A == null) return -1;
+        Node temp = A;
+        while(temp.right != null){
+            temp = temp.right;
+        }
+        return temp.val;
+    }
+
     public static void main(String args[]){
         System.out.println("Finding the Largest and Smallest in BST");
 
@@ -34,6 +44,7 @@ class LargestAndSmallest{
         root.left.left = new Node(2);
         root.left.right = new Node(7);
 
-        System.out.println("Smallest in BST: "+ findSmallest(root));
+        System.out.println("Smallest element in BST: "+ findSmallest(root));
+        System.out.println("Largest element in BST: "+ findLargest(root));
     }
 }
